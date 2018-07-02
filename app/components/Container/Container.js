ef9c0c2bf55e4d31d4ca6438403a8b43b9e46c7d
realPrimoh <priyam.mohanty@gmail.com>
2018-07-01 17:59:58 -0700

    second commit

---
+++ app/components/Container/Container.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+import { View } from 'react-native';
+
+const Container = () => (
+    <View>
+        {children}
+    </View>
+);
+
+<Container>
+    <Text />
+</Container>
---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+EStyleSheet.build({
+    $primaryBlue: '#4F6D7A',
+});
+
+import Home from './screens/Home';
+
+export default () => <Home />;
